fd9a7500ad6437343563a231c59022fc55012c88
Vimmadisetty Naga Sai Thanmai <ugs24095_it.nagasaithanmai@cbit.org.in>
2026-07-26 12:20:14 +0530

    Add authentication middleware to my tasks route

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -6,6 +6,10 @@ const router = express.Router();
 
 const taskController=require("../controllers/taskController");
 
+const { authenticateToken } = require("../middleware/auth");
+
+router.get("/my", authenticateToken, taskController.getMyTasks);
+
 router.get("/",taskController.getTasks);
 
 router.post("/",taskController.createTask);
